a3636a9d15c601f30ae575363928064066baa927
Tomoki Tanimura <tanimu@dhcp199.ht.sfc.keio.ac.jp>
2018-04-18 20:59:42 +0900

    pytorchで書き直した

--- chapter_3/blr.py
+++ chapter_3/blr.py
@@ -1,31 +1,31 @@
-import numpy as np
+import torch
 
 class BayesianLinearRegression(object):
     def __init__(self, M, s_lam):
         self.M = M
         self.s_lam = s_lam
-        self.prior_m = np.zeros(M)
-        self.prior_l_lam = np.eye(M,M)
+        self.prior_m = torch.zeros(M).view(-1,1)
+        self.prior_l_lam = torch.eye(M)
     
     def mapping(self, x):
-        x = x.reshape(1, -1)
-        x_vec = np.concatenate([
-            np.power(x, i) for i in range(self.M)],
-            axis=0)
+        x_vec = torch.ones(len(x)).view(1, -1)
+        for i in range(1, self.M):
+            tmp = torch.pow(x, i).view(1, -1)
+            x_vec = torch.cat((x_vec, tmp), 0)
         return x_vec
-
+            
     def calcurate_posterior(self, tx, ty):
-        tx = tx.T
+        tx = torch.t(tx)
         ynxn = 0
         xnxn = 0
         for x, y in zip(tx, ty):
-            x = x.reshape(1, -1)
+            x = x.view(-1, 1)
             ynxn += y * x
-            xnxn += np.dot(x.T, x)
+            xnxn += torch.mm(x, torch.t(x))
         post_l_lam = self.s_lam * xnxn + self.prior_l_lam
-        inv_post_l_lam = np.linalg.inv(post_l_lam)
-        post_m = self.s_lam * ynxn + np.dot(self.prior_l_lam, self.prior_m)
-        post_m = np.dot(inv_post_l_lam, post_m.T)
+        inv_post_l_lam = torch.inverse(post_l_lam)
+        post_m = self.s_lam * ynxn + torch.mm(self.prior_l_lam, self.prior_m)
+        post_m = torch.mm(inv_post_l_lam, post_m)
 
         self.prior_m = post_m
         self.prior_l_lam = post_l_lam
@@ -37,14 +37,14 @@ class BayesianLinearRegression(object):
             post_m = self.prior_m
             post_l_lam = self.prior_l_lam
 
-        inv_post_l_lam = np.linalg.inv(post_l_lam)
+        inv_post_l_lam = torch.inverse(post_l_lam)
 
-        post_s_lam = 1 / self.s_lam + np.dot(np.dot(xs.T, inv_post_l_lam), xs)
-        post_s_lam = np.diag(post_s_lam).reshape(-1, 1)
+        post_s_lam = 1 / self.s_lam + torch.mm(torch.mm(torch.t(xs), inv_post_l_lam), xs)
+        post_s_lam = torch.diag(post_s_lam).view(-1, 1)
 
-        output_y_u = np.dot(xs.T, post_m)
-        output_y_u_plam = output_y_u + np.sqrt(post_s_lam)
-        output_y_u_mlam = output_y_u - np.sqrt(post_s_lam)
+        output_y_u = torch.mm(torch.t(xs), post_m).view(-1)
+        output_y_u_plam = output_y_u + torch.sqrt(post_s_lam).view(-1)
+        output_y_u_mlam = output_y_u - torch.sqrt(post_s_lam).view(-1)
 
         return output_y_u, output_y_u_plam, output_y_u_mlam
 
--- chapter_3/utils.py
+++ chapter_3/utils.py
@@ -1,22 +1,22 @@
-import numpy as np
+import torch
 import matplotlib.pyplot as plt
 
-def create_dataset(N, function=np.sin):
+def create_dataset(N, function=torch.sin):
     # train_data
-    train_x = np.random.uniform(0, 7, N)
+    train_x = 7 * torch.rand(10)
     train_y = function(train_x)
     # ground_truth
-    gt_x = np.linspace(0, 7)
+    gt_x = torch.linspace(0, 7)
     gt_y = function(gt_x)
 
     return [train_x, train_y], [gt_x, gt_y]
 
 def plotting(ground_truth, train_data, prediction):
-    plt.plot(train_data[0],train_data[1], "o")
-    plt.plot(ground_truth[0], ground_truth[1], color='r')
-    plt.plot(ground_truth[0], prediction[0], color='b')
-    plt.plot(ground_truth[0], prediction[1], linestyle='--', color='c')
-    plt.plot(ground_truth[0], prediction[2], linestyle='--', color='c')
+    plt.plot(list(train_data[0]), list(train_data[1]), "o")
+    plt.plot(list(ground_truth[0]), list(ground_truth[1]), color='r')
+    plt.plot(list(ground_truth[0]), list(prediction[0]), color='b')
+    plt.plot(list(ground_truth[0]), list(prediction[1]), linestyle='--', color='c')
+    plt.plot(list(ground_truth[0]), list(prediction[2]), linestyle='--', color='c')
     
     plt.xlim([-0.5, 7])
     plt.ylim([-3, 3])
