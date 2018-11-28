5322de983590514fcdafa81e25fbfb1432dbb71b
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2018-11-28 22:39:28 +0900

    arange files

--- bayes_intro/chapter_3/torch_blr/blr.py
+++ bayes_intro/chapter_3/torch_blr/blr.py
@@ -6,14 +6,14 @@ class BayesianLinearRegression(object):
         self.s_lam = s_lam
         self.prior_m = torch.zeros(M).view(-1,1)
         self.prior_l_lam = torch.eye(M)
-    
+
     def mapping(self, x):
         x_vec = torch.ones(len(x)).view(1, -1)
         for i in range(1, self.M):
             tmp = torch.pow(x, i).view(1, -1)
             x_vec = torch.cat((x_vec, tmp), 0)
         return x_vec
-            
+
     def calcurate_posterior(self, tx, ty):
         tx = torch.t(tx)
         ynxn = 0
@@ -29,9 +29,9 @@ class BayesianLinearRegression(object):
 
         self.prior_m = post_m
         self.prior_l_lam = post_l_lam
-        
+
         return post_m, post_l_lam
-    
+
     def calcurate_predictive(self, xs, post_m=None, post_l_lam=None):
         if post_m is None and post_l_lam is None:
             post_m = self.prior_m
